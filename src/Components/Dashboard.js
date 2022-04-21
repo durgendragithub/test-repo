fcf15d6c53df8f68b5f0a01998083a3435ae960a
durgendragithub <durgendratirupathi@gmail.com>
2022-04-21 18:40:07 +0530

    my first commit

--- src/Components/Dashboard.js
+++ src/Components/Dashboard.js
@@ -3,9 +3,9 @@ import React from 'react'
 const Dashboard = () => {
   return (
     <div>
-      <h1> i am content of Dashboard page.</h1>
+      <h1> i am a content   of Dashboard page.</h1>
     </div>
   )
-}
+} 
 
 export default Dashboard;
